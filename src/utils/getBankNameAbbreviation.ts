e2daf132606452662725036725e0d007f33ac606
zinny22 <hziny722@naver.com>
2024-04-11 21:42:50 +0900

    refactor: getBankName유틸 함수에 조건 추가

--- src/utils/getBankNameAbbreviation.ts
+++ src/utils/getBankNameAbbreviation.ts
@@ -1,8 +1,11 @@
-export default function getBankNameAbbreviation(bankName: string) {
+export default function getBankNameAbbreviation(
+  bankName: string,
+  isShowSuffix?: boolean
+) {
   return bankName === "한국스탠다드차타드은행"
     ? "SC제일"
     : bankName
-        .replace("은행", "")
+        .replace(`${isShowSuffix ? "은행" : ""}`, "")
         .replace("주식회사", "")
         .replace(" ", "")
         .replace("중소", "");
