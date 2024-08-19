fa3dae90a927df320635fd65a73ed5eeea036183
장혜진/상품DX지원파트 <hziny722@gsretail.com>
2024-08-20 08:27:27 +0900

    fix : push 테스트

--- src/components/Pages/HomeDetail.tsx
+++ src/components/Pages/HomeDetail.tsx
@@ -1,10 +1,10 @@
-import Top4List from "../Organisms/Top4List";
+import Top4List from '../Organisms/Top4List';
 
 function HomeDetail() {
   return (
-    <div className="h-[100vh] w-full pt-[58px]">
+    <div className="h-[100vh] min-w-[400px] w-full pt-[58px]">
       <section className="flex gap-x-6 px-[180px]">
-        <div className="flex-1 bg-[#2972FF] rounded-tr-xl rounded-br-xl p-12 text-white text-[28px] font-bold cursor-pointer">
+        <div className="flex-1 bg-[#2972FF] rounded-xl p-12 text-white text-[28px] font-bold cursor-pointer">
           나에게
           <br />
           맞는 예적금은?
