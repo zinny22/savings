5782796c7040ca28c1c803a42d8aac4d46039d80
장혜진/상품DX지원파트 <hziny722@gsretail.com>
2024-08-22 16:46:59 +0900

    feature : toggle 추가

--- src/components/Pages/HomeDetail.tsx
+++ src/components/Pages/HomeDetail.tsx
@@ -1,17 +1,9 @@
-import Top4List from "../Organisms/Top4List";
+import Top4List from '../Organisms/Top4List';
 
 function HomeDetail() {
   return (
     <div className="h-[100vh] w-full pt-[58px]">
-      <section className="flex gap-x-6 px-[180px]">
-        <div className="flex-1 bg-[#2972FF] rounded-tr-xl rounded-br-xl p-12 text-white text-[28px] font-bold cursor-pointer">
-          나에게
-          <br />
-          맞는 예적금은?
-        </div>
-
-        <Top4List />
-      </section>
+      <Top4List />
     </div>
   );
 }
